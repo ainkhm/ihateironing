10bf5b9c62e8dc0f6fa7394d580271d79652c97e
Andrii <A.I.Khmelovsky@gmail.com>
2022-10-09 12:05:07 +0300

    Added mutations functions

--- components/services/api.ts
+++ components/services/api.ts
@@ -2,23 +2,23 @@ import axios from 'axios';
 
 const baseURL = "https://virtserver.swaggerhub.com/I425/ReactJS/1.0.0";
 
-
-const getQuery = async <T>(url = ""): Promise<T> => {
+export const getQuery = async <T>(url = ""): Promise<T> => {
   const { data } = await axios(baseURL + url);
   return data.json();
 };
 
-const postQuery = async (url = "", body = {}) => {
+export const postQuery = async (url = "", body = {}) => {
   const { data } = await axios.post(baseURL + url, body);
   return data;
 };
 
-const deleteQuery = async (url = "") => {
+export const deleteQuery = async (url = "") => {
   const { data } = await axios.delete(baseURL + url);
   return data;
 };
 
-const putQuery = async (url = "", data = {}) => {
+export const putQuery = async (url = "", data = {}) => {
   const response = await axios.put(baseURL + url, data);
   return response;
 };
+
---
+++ components/services/mutations.ts
@@ -0,0 +1,27 @@
+
+import { Item, Product } from "../common/types";
+import { postQuery, getQuery, deleteQuery, putQuery } from './api'
+
+export const addItem = (data: Pick<Item, "id" | "quantity">) => postQuery("/cart", data);
+
+export const showItems = () =>
+getQuery<
+  ({
+    item_id: Item["id"];
+  } & Omit<Item, "id">)[]
+>("/cart");
+
+export const resetCart = () => deleteQuery("/cart");
+
+export const deleteYourItem = (itemId: Item["id"]) =>
+deleteQuery(`/cart/${itemId}`);
+
+export const updateItem = (
+itemId: Item["id"],
+quantity: Item["quantity"]
+) => putQuery(`/cart/${itemId}?quantity=${quantity}`);
+
+export const getItems = (favorites: boolean = false) =>
+getQuery<Product[]>(`/items?favorites=${favorites}`);
+
+export const getItem = (id: Product["id"]) => getQuery<Product>(`/items/${id}}`);
